c48cad58fb2212375f2e9e9f081d65d0721071fc
Adamm00 <adamm_is@hotmail.com>
2017-05-08 08:32:14 +1000

    Revert firewall-restart fix, needs more debugging

--- firewall-start.sh
+++ firewall-start.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-	if [ X"`ps | grep firewall-start | wc -l`" != X"3" ]
-	then
-		exit # firewall-start fix
-	else
-		sleep 10
-		cru a Firewall_save "0 * * * * /jffs/scripts/firewall save"
-		sh /jffs/scripts/firewall start
-	fi
+cru a Firewall_save "0 * * * * /jffs/scripts/firewall save"
+sh /jffs/scripts/firewall start
