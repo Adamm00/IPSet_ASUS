0afe5594b67002241b010d4d70e1453b2e33465d
Adamm00 <adamm_is@hotmail.com>
2017-05-08 07:20:05 +1000

    Fix firewall-start multiple events
    firewall-start now gets triggered multiple times causing scripts to also be triggered, detects if already running and exits if so.

--- firewall-start.sh
+++ firewall-start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-cru a Firewall_save "0 * * * * /jffs/scripts/firewall save"
-sh /jffs/scripts/firewall
+	if [ X"`ps | grep firewall-start | wc -l`" != X"3" ]
+	then
+		exit # firewall-start fix
+	else
+		sleep 10
+		cru a Firewall_save "0 * * * * /jffs/scripts/firewall save"
+		sh /jffs/scripts/firewall start
+	fi
