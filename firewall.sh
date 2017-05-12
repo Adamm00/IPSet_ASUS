74191e08f4be1b4f34eb2cb41752b77d649268a9
Adamm00 <adamm_is@hotmail.com>
2017-05-13 03:21:48 +1000

    Clear logs if older then 7 days (limited JFFS space)

--- firewall.sh
+++ firewall.sh
@@ -9,7 +9,7 @@
 #			                   __/ |                             				    #
 # 			                  |___/                              				    #
 #													    #
-## - 13/05/2017 -		   Asus Firewall Addition By Adamm v3.7.7				    #
+## - 13/05/2017 -		   Asus Firewall Addition By Adamm v3.7.8				    #
 ## 				   https://github.com/Adamm00/IPSet_ASUS				    #
 ###################################################################################################################
 ###			       ----- Make Sure To Edit The Following Files -----				  #
@@ -134,6 +134,7 @@ Unban_PrivateIP () {
 }
 
 Purge_Logs () {
+		find /jffs/skynet.log -mtime +7 -type f -delete
 		cat /tmp/syslog.log-1 | sed '/BLOCKED -/!d' >> /jffs/skynet.log
 		sed -i '/BLOCKED -/d' /tmp/syslog.log-1
 		cat /tmp/syslog.log | sed '/BLOCKED -/!d' >> /jffs/skynet.log
@@ -436,7 +437,7 @@ case $1 in
 			echo '(DST=127\.)|(DST=10\.)|(DST=172\.1[6-9]\.)|(DST=172\.2[0-9]\.)|(DST=172\.3[0-1]\.)|(DST=192\.168\.)|(DST=0.)|(DST=169\.254\.)'
 		}
 		if [ -f /jffs/skynet.log ] && [ "$(wc -l /jffs/skynet.log | awk '{print $1}')" != "0" ]; then
-			echo "Debug Data Detected in /jffs/skynet.log"
+			echo "Debug Data Detected in /jffs/skynet.log - $(ls -lh /jffs/skynet.log | awk '{print $5}')"
 		else
 			echo "No Debug Data Detected - Make Sure Debug Mode Is Enabled To Compile Stats"
 			exit
