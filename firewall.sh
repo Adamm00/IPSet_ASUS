85b76b45fc75363a66df64441deedf911f570499
Adamm00 <adamm_is@hotmail.com>
2017-06-17 22:55:39 +1000

    exFAT support

--- firewall.sh
+++ firewall.sh
@@ -9,7 +9,7 @@
 #			                   __/ |                             				    #
 # 			                  |___/                               				    #
 #													    #
-## - 14/06/2017 -		   Asus Firewall Addition By Adamm v4.9.9				    #
+## - 17/06/2017 -		   Asus Firewall Addition By Adamm v4.9.9				    #
 ## 				   https://github.com/Adamm00/IPSet_ASUS				    #
 #############################################################################################################
 
@@ -911,11 +911,11 @@ case "$1" in
 			echo
 			echo "USB Installation Selected"
 			echo "Compadible Devices To Install Are;"
-			mount | grep -E 'ext2|ext3|ext4|tfat' | awk '{print $3" - ("$1")"}'
+			mount | grep -E 'ext2|ext3|ext4|tfat|exfat' | awk '{print $3" - ("$1")"}'
 			echo
 			echo "Please Type Device Label - eg /tmp/mnt/Main"
 			read -r device
-			if ! mount | grep -E 'ext2|ext3|ext4|tfat' | awk '{print " "$3" "}' | grep -wq " $device "; then
+			if ! mount | grep -E 'ext2|ext3|ext4|tfat|exfat' | awk '{print " "$3" "}' | grep -wq " $device "; then
 				echo "Error - Input Not Recognised, Exiting Installation"
 				rm -rf /tmp/skynet.lock
 				exit
