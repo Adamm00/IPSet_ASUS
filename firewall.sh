d5436f3a1477cd77e084627d9178feeaeaaf68d6
Adamm00 <adamm_is@hotmail.com>
2017-07-16 22:26:03 +1000

    Use https for abuseat list

--- firewall.sh
+++ firewall.sh
@@ -9,7 +9,7 @@
 #			                    __/ |                             				    #
 #			                   |___/                              				    #
 #													    #
-## - 15/07/2017 -		   Asus Firewall Addition By Adamm v5.0.4				    #
+## - 16/07/2017 -		   Asus Firewall Addition By Adamm v5.0.5				    #
 ##				   https://github.com/Adamm00/IPSet_ASUS				    #
 #############################################################################################################
 
@@ -1029,7 +1029,7 @@ case "$1" in
 			Unload_DebugIPTables
 			Unload_IPSets
 			sed -i '\~ Skynet ~d' /jffs/scripts/firewall-start
-			rm -rf "${location}/scripts/ipset.txt" "${location}/scripts/malwarelist.txt" "${location}/scripts/countrylist.txt" "${location}/skynet.log" "/jffs/scripts/firewall"
+			rm -rf "${location}/scripts/ipset.txt" "${location}/scripts/malwarelist.txt" "${location}/scripts/countrylist.txt" "${location}/skynet.log" "/jffs/shared-Skynet-whitelist" "/jffs/scripts/firewall"
 			iptables -t raw -F
 			service restart_firewall
 			exit 0
