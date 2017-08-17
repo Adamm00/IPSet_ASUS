d1647e11e6c4db1bfcd15b6ea700897e430b81ca
Adamm00 <adamm_is@hotmail.com>
2017-08-17 20:37:03 +1000

    Better fix for 223e609664ac4e068ef98535a4b0a2a0a96f814f

--- firewall.sh
+++ firewall.sh
@@ -704,7 +704,7 @@ case "$1" in
 				echo "Boot Args; $(grep -F "Skynet" /jffs/scripts/firewall-start | cut -c 4- | cut -d '#' -f1)"
 				if [ -w "$location" ]; then $grn "Install Dir Writeable"; else $red "Can't Write To Install Dir"; fi
 				if grep -qF "Skynet" /jffs/scripts/firewall-start; then $grn "Startup Entry Detected"; else $red "Startup Entry Not Detected"; fi
-				if [ -f "/tmp/skynet.lock" ]; then if [ ! -d "/proc/$(cat /tmp/skynet.lock)" ]; then $grn "No Lock File Found"; else $red "Lock File Detected (pid=$(cat /tmp/skynet.lock))"; fi; else $grn "No Lock File Found"; fi
+				if [ -f "/tmp/skynet.lock" ] && [ -d "/proc/$(cat /tmp/skynet.lock)" ]; then $red "Lock File Detected (pid=$(cat /tmp/skynet.lock))"; else $grn "No Lock File Found"; fi
 				if cru l | grep -qF "Skynet"; then $grn "Cronjobs Detected"; else $red "Cronjobs Not Detected"; fi
 				if [ -f /lib/modules/2.6.36.4brcmarm/kernel/net/netfilter/ipset/ip_set_hash_ipmac.ko ]; then $grn "IPSet Supports Comments"; else $red "IPSet Doesn't Support Comments - Please Update To 380.68_alpha1 / V26E3 Or Newer Firmware"; fi
 				if iptables -nL | grep -F "LOG" | grep -qF "NEW BAN"; then $grn "Autobanning Enabled"; else $red "Autobanning Disabled"; fi
