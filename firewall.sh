6c4841fd05ddb08b9285d69ea5128822e6b4fa33
Adamm00 <adamm_is@hotmail.com>
2019-01-12 01:45:49 +1000

    Unnessesary check

--- firewall.sh
+++ firewall.sh
@@ -650,7 +650,7 @@ Refresh_AiProtect () {
 			fi
 			if [ -f /opt/bin/opkg ] && [ -f /opt/bin/sqlite3 ]; then
 				sed "\\~add Skynet-Blacklist ~!d;\\~BanAiProtect~!d;s~ comment.*~~;s~add~del~g" "$skynetipset" | ipset restore -!
-				sqlite3 /jffs/.sys/AiProtectionMonitor/AiProtectionMonitor.db "SELECT src FROM monitor;" | awk '!x[$0]++' | Filter_IP | Filter_PrivateIP | grep -v ":" | awk '{printf "add Skynet-Blacklist %s comment \"BanAiProtect\"\n", $1 }' | ipset restore -!			
+				sqlite3 /jffs/.sys/AiProtectionMonitor/AiProtectionMonitor.db "SELECT src FROM monitor;" | awk '!x[$0]++' | Filter_IP | Filter_PrivateIP | awk '{printf "add Skynet-Blacklist %s comment \"BanAiProtect\"\n", $1 }' | ipset restore -!			
 				sqlite3 /jffs/.sys/AiProtectionMonitor/AiProtectionMonitor.db "SELECT dst FROM monitor;" | awk '!x[$0]++' | Filter_OutIP | grep -v ":" | while IFS= read -r "domain"; do
 					for ip in $(Domain_Lookup "$domain" | Filter_PrivateIP); do
 						ipset -q -A Skynet-Blacklist "$ip" comment "BanAiProtect: $domain"
